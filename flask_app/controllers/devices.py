53c089194f12e02182be1a528b6dbb5862bc6d67
Jennifer Stewart <jlgregg1@gmail.com>
2022-07-27 14:08:01 -0700

    defined routes

--- flask_app/controllers/devices.py
+++ flask_app/controllers/devices.py
@@ -2,3 +2,40 @@ from flask import Flask, flash, session, render_template, redirect, request
 from flask_app import app
 from flask_app.models import user, device
 from flask_app.controllers import users
+
+@app.route("/learn")
+def learn():
+    return render_template("learn.html")
+
+@app.route("/find")
+def find():
+    return render_template("find.html")
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
+#hidden
+@app.route("/add_to_db")
+def add_to_db():
+    pass
+    # return redirect("/dashboard")
+
+@app.route("/add_to_saved_devices")
+def add_to_saved_devices():
+    pass
+    # return redirect("/dashboard")
+
+@app.route("/delete_from_db")
+def delete_from_db():
+    pass
+    # return redirect("/dashboard")
+
+@app.route("remove_from_saved")
+def remove_from_saved():
+    pass
+    # return redirect("/dashboard")
--- flask_app/models/device.py
+++ flask_app/models/device.py
@@ -7,3 +7,23 @@ from flask_app.controllers import devices, users
 from flask_app.models import user
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
+
+class Device:
+    db = "users_devices"
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.type = data['type']
+        self.zip_code = data['zip_code']
+        self.comments = data['comments']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @staticmethod
+    def validate_device():
+        pass
+
+    @classmethod
+    def add_to_db():
+        pass
+
