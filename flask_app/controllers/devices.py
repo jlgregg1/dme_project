e30b5a3b9449b65fc80a5c1e4eea44217028ff43
Jennifer Stewart <jlgregg1@gmail.com>
2022-07-27 14:45:08 -0700

    added routes to render templates, add

--- flask_app/controllers/devices.py
+++ flask_app/controllers/devices.py
@@ -13,17 +13,55 @@ def find():
 
 @app.route("/post")
 def post():
+    if "user_id" not in session: #do not allow access to create page if not logged in (if user ID not in session)
+        return redirect ("/")
     return render_template("post.html")
 
 @app.route("/results")
 def results():
+    if "user_id" not in session: #do not allow access to create page if not logged in (if user ID not in session)
+        return redirect ("/")
     return render_template("results.html")
 
+@app.route("/edit/<int:id>")
+def edit(id):
+    if "user_id" not in session: #do not allow access to create page if not logged in (if user ID not in session)
+        return redirect ("/")
+    data = {
+        "id" : id
+    }
+    return render_template("edit.html") #will need to pass in correct device info
+
 #hidden
-@app.route("/add_to_db")
-def add_to_db():
+@app.route("/find_device", methods = ["POST"])
+def find_device():
     pass
-    # return redirect("/dashboard")
+
+@app.route("/edit_in_db/<int:id>", methods = ['POST'])
+def edit_in_db(id):
+    if not device.Device.validate_device(request.form):
+        return redirect(f'edit_in_db/{id}')
+    data = {
+        'id' : id,
+        'type' : request.form['type'],
+        'zip_code' : request.form['zip_code'],
+        'comments' : request.form['comments']
+    }
+    device.Device.edit_in_db(data)
+    return redirect('/dashboard')
+
+@app.route("/add_to_db", methods = ["POST"])
+def add_to_db():
+    if not device.Device.validate_device(request.form):
+        return redirect('/post')
+    data = {
+        'type' : request.form['type'],
+        'zip_code' : request.form['zip_code'],
+        'comments' : request.form['comments'],
+        'user_id' : session['user_id'] #track who posted it
+    }
+    device.Device.add_to_db(data)
+    return redirect('/dashboard')
 
 @app.route("/add_to_saved_devices")
 def add_to_saved_devices():
@@ -35,7 +73,7 @@ def delete_from_db():
     pass
     # return redirect("/dashboard")
 
-@app.route("remove_from_saved")
+@app.route("/remove_from_saved")
 def remove_from_saved():
     pass
     # return redirect("/dashboard")
