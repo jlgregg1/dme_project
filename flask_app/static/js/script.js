5a03b6daec3cca777bbe9182a2a60b19df9185ac
Jennifer Stewart <jlgregg1@gmail.com>
2022-08-02 21:26:07 -0700

    added hover event, added logo, added css

---
+++ flask_app/static/js/script.js
@@ -0,0 +1,8 @@
+function magnifyImage(element){
+    element.style.width = "300px";
+    element.style.height = 'auto';
+}
+function revertImage(element){
+    element.style.width = "100px";
+    element.style.height = "auto";
+}
