8c3edebfcc2eb574cc28ac7b83d3208f4307791b
Jennifer Stewart <jlgregg1@gmail.com>
2022-08-08 18:23:49 -0700

    arrow for select menus, color schemes for cards

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -6,3 +6,6 @@ function revertImage(element){
     element.style.width = "100px";
     element.style.height = "auto";
 }
+function displayInfo(element){
+    element.innerHTML = "<p>For wheelchair include seat width and seat depth. For canes and walkers include manufacturer's recommended height range for person using the device.</p>";
+}
